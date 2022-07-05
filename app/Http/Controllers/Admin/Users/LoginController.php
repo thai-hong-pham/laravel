3044082233851f3ce748f40b292bed43c35314e5
thai-hong-pham <thaihongpham0212@gmail.com>
2022-07-05 17:00:30 +0700

    Initial commit

---
+++ app/Http/Controllers/Admin/Users/LoginController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Users;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class LoginController extends Controller
+{
+    public function loginForm()
+    {
+        return view('login');
+    }
+
+    public function home()
+    {
+        return view('main');
+    }
+
+    public function saveLogin(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+        if (Auth::attempt($credentials, $request->input('remember'))) {
+            $user = Auth::user();
+            $user->save();
+            $request->session()->regenerate();
+            return redirect(route('admin'));
+        }
+        Session::flash('error','Email hoặc password không chính xác');
+            return redirect()->back();
+        
+    }
+}
